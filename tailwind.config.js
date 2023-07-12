cd3f6907f02e7ad12c70aede8dd2c266230df29c
Pedro Henrique Bessa <99356444+Pedrohsbessa@users.noreply.github.com>
2023-07-12 16:47:58 -0300

    changes is html and css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      flexBasis: {
+        '1/9': '11%',
+        '1/10': '10%'
+      },
+      height: {
+        'same': '100px',
+      }
+    },
   },
   plugins: [],
 }
